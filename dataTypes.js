d3a3f494067f8783dd4b0f5e1d9fc607ac06ef6f
FAIZANE RASUL <140742543+rasul0055@users.noreply.github.com>
2024-06-27 11:17:09 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a new version
+
+alert( 4 + 4) // we are using node js, not browser
+
+let name = "faizan";
+let age = 22;
+let isLoggedIn = true;
+
+
+// number => 2 to pwr 53;
+// bigint
+// string => ""
+// boolean => true/false
+// nullc=> standalone
+// undefined
+// symbol
+
+// object
+
+console.log(typeof undefined); //unudefined
+console.log(typeof null); //object
---
+++ var.js
@@ -0,0 +1,15 @@
+const accountId = 2376731;
+let accouuntEmail = "faizan@gmail.com";
+var accountPass = "1234";
+
+accountCity = "Patna";
+
+// accountId = 7; const is unchangeable
+
+accouuntEmail = "faiz@gamil.com";
+accountPass = "37673";
+accountCity = "nalanda";
+
+console.log(accountId);
+
+console.table([accouuntEmail, accountPass, accountCity])
